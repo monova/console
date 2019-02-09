3ec3f8143ab67b4e8ee175a2b487854ff49bae52
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-09 23:19:22 +0300

    Added new pages.

---
+++ src/Monova.Web/ClientApp/components/pages/monitoring/save.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <page-head
+      icon="plus"
+      title="New Monitoring"
+    />
+    <button @click="save">
+      Save
+    </button>
+  </div>
+</template>
+
+<script>
+import service from "service/monitoring";
+export default {
+  methods: {
+    async save() {
+      var result = service.save({
+        name: "selcuk-ermaya",
+        url: "https://selcukermaya.com/"
+      });
+    }
+  }
+};
+</script>
