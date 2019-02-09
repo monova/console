527d2e63d021d4925080c7ed110b69a14a44b340
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-09 23:19:28 +0300

    Added page-head component.

---
+++ src/Monova.Web/ClientApp/components/shared/page-head.vue
@@ -0,0 +1,24 @@
+<template>
+  <h4 class="font-weight-bold py-3 mb-4">
+    <icon v-if="icon" :icon="icon" /> <span class="text-muted font-weight-light">{{ prefix }} /</span> {{ title }}
+  </h4>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      default: "",
+      type: String
+    },
+    prefix: {
+      default: "Monova",
+      type: String
+    },
+    icon: {
+      default: "Monova",
+      type: String
+    }
+  }
+};
+</script>
