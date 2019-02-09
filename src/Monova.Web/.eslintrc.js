16019e98263f27f40b173eea481b55005d073043
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-09 23:12:53 +0300

    ESLint changed.

--- src/Monova.Web/.eslintrc.js
+++ src/Monova.Web/.eslintrc.js
@@ -18,6 +18,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'indent': [
+      2, "tab"
+    ],
+    'no-tabs': 0,
+    'semi': [1, 'always']
   }
 }
