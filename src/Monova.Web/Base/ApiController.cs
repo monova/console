9008d375efe2051bd466723e4eb7bd5c50efa201
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-10 20:04:16 +0300

    Added Succes and Error return value.

--- src/Monova.Web/Base/ApiController.cs
+++ src/Monova.Web/Base/ApiController.cs
@@ -1,9 +1,49 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Monova.Entity;
 
 namespace Monova.Web
 {
     [Route("api/v1/[controller]")]
     public class ApiController : SecureDbController
     {
+        private UserManager<MVDUser> _userManager;
+        public UserManager<MVDUser> UserManager => _userManager ?? (UserManager<MVDUser>)HttpContext?.RequestServices.GetService(typeof(UserManager<MVDUser>));
+
+        public IActionResult Success(string message = default(string), object data = default(object), int code = 200)
+        {
+            return Ok(
+                new MVReturn()
+                {
+                    Success = true,
+                    Message = message,
+                    Data = data,
+                    Code = code
+                }
+            );
+        }
+
+        public IActionResult Error(string message = default(string), string internalMessage = default(string), object data = default(object), int code = 400, List<MVReturnError> errorMessages = null)
+        {
+            var rv = new MVReturn()
+            {
+                Success = false,
+                Message = message,
+                InternalMessage = internalMessage,
+                Data = data,
+                Code = code
+            };
+
+            if (rv.Code == 500)
+                return StatusCode(500, rv);
+            if (rv.Code == 401)
+                return Unauthorized();
+            if (rv.Code == 403)
+                return Forbid();
+
+            return BadRequest(rv);
+        }
     }
 }
