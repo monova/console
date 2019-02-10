77b1cc09dd3288fc0101ee3982f8a17677d014a1
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-10 20:36:46 +0300

    Bulding UI and API for monitoring.

--- src/Monova.Web/ClientApp/components/pages/monitoring/save.vue
+++ src/Monova.Web/ClientApp/components/pages/monitoring/save.vue
@@ -4,6 +4,10 @@
       icon="plus"
       title="New Monitoring"
     />
+    <input
+      type="text"
+      v-model="model.name"
+    />
     <button @click="save">
       Save
     </button>
@@ -13,11 +17,17 @@
 <script>
 import service from "service/monitoring";
 export default {
+  data() {
+    return {
+      model: {
+        name: ""
+      }
+    };
+  },
   methods: {
     async save() {
       var result = service.save({
-        name: "selcuk-ermaya",
-        url: "https://selcukermaya.com/"
+        name: this.model.name
       });
     }
   }
--- src/Monova.Web/Controllers/MonitoringController.cs
+++ src/Monova.Web/Controllers/MonitoringController.cs
@@ -1,30 +1,42 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Monova.Entity;
 
 namespace Monova.Web.Controllers
 {
     public class MonitoringController : ApiController
     {
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            return Json(new
-            {
-                Success = true,
-                Message = "Hi there!"
-            });
+            var list = await Db.Monitors.ToListAsync();
+            return Success(null, list);
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]MonitoringModel value)
+        public async Task<IActionResult> Post([FromBody]MVDMonitor value)
         {
-            return Forbid();
-            // return Json(new
-            // {
-            //     Success = true,
-            //     Message = "I saved it.",
-            //     Data = value
-            // });
+            if (string.IsNullOrEmpty(value.Name))
+            {
+                return Error("Name is required.");
+            }
+            var dataObject = new MVDMonitor
+            {
+                CreatedDate = DateTime.UtcNow,
+                Name = value.Name
+            };
+            Db.Monitors.Add(dataObject);
+            var result = await Db.SaveChangesAsync();
+            if (result > 0)
+                return Success("Monitoring saved successfully.", new
+                {
+                    Id = dataObject.MonitorId
+                });
+            else
+                return Error("Something is wrong with your model.");
         }
     }
 
