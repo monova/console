eb97b59f6329eb6495d0f4d3622972f12e3247b0
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-12 22:48:01 +0300

    Added new component.

---
+++ src/Monova.Web/ClientApp/components/input/text.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card mb-4">
+    <h6 class="card-header">
+      <div>{{title}}</div>
+    </h6>
+    <div class="card-body"><input
+        type="text"
+        :placeholder="placeholder"
+        class="form-control"
+        v-on:input="$emit('input', $event.target.value)"
+      ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      default: "",
+      type: String
+    },
+    title: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
