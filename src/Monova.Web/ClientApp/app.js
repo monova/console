50adbac90aebb3da919ce19b8890cb5a09bb6183
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-12 22:47:41 +0300

    Added vue loader for fancy pages.

--- src/Monova.Web/ClientApp/app.js
+++ src/Monova.Web/ClientApp/app.js
@@ -2,17 +2,31 @@ import Vue from 'vue';
 import axios from 'axios';
 import router from './router/index';
 import store from './store';
-import { sync } from 'vuex-router-sync';
+import {
+  sync
+} from 'vuex-router-sync';
 import App from 'components/root/app-root';
-import { FontAwesomeIcon } from './icons';
+import {
+  FontAwesomeIcon
+} from './icons';
 import PageHead from 'components/shared/page-head';
 import Notifications from 'vue-notification';
+
+import VueContentPlaceholders from "vue-content-placeholders";
+
+Vue.use(VueContentPlaceholders)
+
+// Input Controls
+import MVIText from 'components/input/text';
 
 Vue.use(Notifications);
 
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon);
 Vue.component('page-head', PageHead);
+
+// Registration for Input Controls
+Vue.component('mvi-text', MVIText);
 
 Vue.prototype.$http = axios;
 
