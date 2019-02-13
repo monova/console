5451118cccc4142cf1a8f314b3e1353a396dd24b
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-13 20:40:16 +0300

    Model updated for save.

--- src/Monova.Web/Models/Client/MVMMonitorSave.cs
+++ src/Monova.Web/Models/Client/MVMMonitorSave.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace Monova.Web
 {
     public class MVMMonitorSave
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
     }
