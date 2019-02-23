ef41194401e95bcee7a5b5bfd43ed39543b1a405
Selçuk Ermaya <selcukermaya@gmail.com>
2019-02-23 23:06:14 +0300

    Added new ui component.

---
+++ src/Monova.Web/ClientApp/components/shared/monitor-status.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <span :class="'badge badge-'+badgeClass">{{title}}</span>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    title: {
+      default: "",
+      type: String
+    },
+    status: {
+      default: 0,
+      type: Number
+    }
+  },
+
+  computed: {
+    badgeClass() {
+      switch (this.status) {
+        case 0:
+          return "dark";
+        case 2:
+          return "primary";
+        case 3:
+          return "success";
+        case 4:
+          return "warning";
+        case 5:
+        case 6:
+          return "danger";
+        default:
+          return "light";
+      }
+    }
+  }
+};
+</script>
