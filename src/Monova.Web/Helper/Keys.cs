9d6bf10aa11b62bb14881fd813cf4fc8853d85f8
Selçuk Ermaya <selcukermaya@gmail.com>
2019-03-16 21:59:14 +0300

    Added config keys.

---
+++ src/Monova.Web/Helper/Keys.cs
@@ -0,0 +1,9 @@
+namespace Monova.Web
+{
+    public static class Keys
+    {
+        public const string ConnectionString = "MONOVA_CONNECTIONSTRING";
+        public const string StripeApiKey = "STRIPE_API_KEY";
+        public const string StripePublicKey = "STRIPE_PUBLIC_KEY";
+    }
+}
