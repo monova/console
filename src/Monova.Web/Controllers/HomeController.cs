6a270afee347e3b96adecb3378a97d00cc0f81e6
Selçuk Ermaya <selcukermaya@gmail.com>
2019-03-24 10:26:21 +0300

    Removed unnecessary lines.

--- src/Monova.Web/Controllers/HomeController.cs
+++ src/Monova.Web/Controllers/HomeController.cs
@@ -1,20 +1,20 @@
+using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Monova.Entity;
 
 namespace Monova.Web.Controllers
 {
-    public class HomeController : SecureController
-    {
-        public IActionResult Index()
-        {
-            System.Console.WriteLine("Hola!");
-            return View();
-        }
+	public class HomeController : SecureController
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Error()
-        {
-            return View();
-        }
-    }
+		public IActionResult Error()
+		{
+			return View();
+		}
+	}
 }
